ed25bfe8f55ff2d6aa126f5097d698aa2086080d
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-07-20 02:30:11 -0700

    fix webtty build

--- flake.nix
+++ flake.nix
@@ -7,12 +7,30 @@
         f system (import nixpkgs {inherit system;}));
   in {
     packages = forAllSystems (
-      system: pkgs: rec {
+      system: pkgs: let
+        webClient = pkgs.callPackage ./web-client/web-client.nix {};
+      in rec {
+        inherit webClient;
         webtty = pkgs.buildGoModule {
           name = "webtty";
           version = "flake-latest";
           src = ./.;
-          vendorHash = "sha256-qhQ+n54AHrD7b4gac6yV7d7A6SqTeyOYQhcENlje/gY=";
+          ldflags = "-X 'main.ServePath=${webClient}'";
+          # by default, buildGoModule does a lot of magic
+          # that isn't compatible with this repo
+          # specifically, the thing where it tries to build all
+          # "submodules". For some reason, `go build ./web-client`
+          # doesn't work.
+          buildPhase = ''
+            go build -p "$NIX_BUILD_CORES" --ldflags="$ldflags"
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            mv ./webtty $out/bin
+          '';
+          # Don't run any tests
+          checkPhase = "";
+          vendorHash = "sha256-31Qg8ond4wqo1qokk4Adlv1tuqwNoWYm61Y24DRq48A=";
         };
 
         default = webtty;
